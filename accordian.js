e1ae448d5600734bf61d312d1201dd88416f9848
rohanghiya <rohanghiya@icloud.com>
2017-07-15 20:02:05 -0700

    Initial Commit
    Dummy Frame without any content

---
+++ accordian.js
@@ -0,0 +1,25 @@
+
+$(function() {
+  $('.accordian-toggle3').on('click',function() {
+	$('.accordian3').toggleClass('open');
+  });
+});
+
+$(function() {
+  $('.accordian-toggle2').on('click',function() {
+	$('.accordian2').toggleClass('open');
+  });
+});
+
+$(function() {
+  $('.accordian-toggle1').on('click',function() {
+	$('.accordian1').toggleClass('open');
+  });
+});
+
+$(function() {
+  $('.accordian-toggle').on('click',function() {
+	$('.accordian').toggleClass('open');
+  });
+});
+
